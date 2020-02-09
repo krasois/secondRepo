9aa99d1e426b77e50d9f79241db6bfad9d50b635
Krasimir Todorov <krasimir.c.todorov@gmail.com>
2020-02-09 20:27:16 +0200

    Renamed method name

--- src/com/vsc/strings/Testing.java
+++ src/com/vsc/strings/Testing.java
@@ -1,15 +1,15 @@
 package com.vsc.strings;
 
-import java.util.Scanner;
-
 public class Testing {
 
     public static void main(String[] args) {
         String[] data = {"Ivan Ivanov", "Bulgaria", "Sofia", "Ulica", "23020"};
-        System.out.println(formatLetter(data));
+        System.out.println(formatLetter2(data));
+
+        System.out.println("asdasdasdasdas");
     }
 
-    public static String formatLetter(String[] data) {
+    public static String formatLetter2(String[] data) {
         return String.format("%s\n%s\n%s\n%s\n%s", data[0], data[1],
                 data[2], data[3], data[4]);
     }
